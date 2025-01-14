93fe572ed21aac6145369eb00accff4f4f19ebf1
Brynna Newlie <bryn.lizabeth@gmail.com>
2025-01-14 09:57:46 -0500

    fix

--- src/WorkshopDemo.Core.Tests/Common/FileServiceTests.cs
+++ src/WorkshopDemo.Core.Tests/Common/FileServiceTests.cs
@@ -16,7 +16,7 @@ public class FileServiceTests
 
         var result = _fileService.GetFileContents(fileName);
 
-        rresult.Should().Be("Hello world");
+        result.Should().Be(fileContents);
     }
 
     [Fact]
